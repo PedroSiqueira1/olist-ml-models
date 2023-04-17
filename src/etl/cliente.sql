f454ecb1e8e764d67d4dd806da65dc6950615ad9
Pedro Siqueira <Pedro Siqueira>
2023-04-17 21:13:02 +0000

    Add Pedidos por estados

--- src/etl/cliente.sql
+++ src/etl/cliente.sql
@@ -27,11 +27,51 @@ WITH tb_join AS (
 
   WHERE t1.dtPedido < '2018-01-01'
   AND t1.dtPedido >= add_months('2018-01-01', -6)
-  AND idVendedor IS NOT NULL
+  
+), tb_group AS (
+
+  SELECT 
+  idVendedor,
+  count(distinct descUF) AS qtdUFsPedidos,
+  count(CASE WHEN descUF = 'AC' then idPedido end)/count(distinct idPedido) as pct_PedidoAC,  
+  count(CASE WHEN descUF = 'AL' then idPedido end)/count(distinct idPedido) as pct_PedidoAL,  
+  count(CASE WHEN descUF = 'AM' then idPedido end)/count(distinct idPedido) as pct_PedidoAM,  
+  count(CASE WHEN descUF = 'AP' then idPedido end)/count(distinct idPedido) as pct_PedidoAP,  
+  count(CASE WHEN descUF = 'BA' then idPedido end)/count(distinct idPedido) as pct_PedidoBA,  
+  count(CASE WHEN descUF = 'CE' then idPedido end)/count(distinct idPedido) as pct_PedidoCE,  
+  count(CASE WHEN descUF = 'DF' then idPedido end)/count(distinct idPedido) as pct_PedidoDF,  
+  count(CASE WHEN descUF = 'ES' then idPedido end)/count(distinct idPedido) as pct_PedidoES,  
+  count(CASE WHEN descUF = 'GO' then idPedido end)/count(distinct idPedido) as pct_PedidoGO,  
+  count(CASE WHEN descUF = 'MA' then idPedido end)/count(distinct idPedido) as pct_PedidoMA,  
+  count(CASE WHEN descUF = 'MG' then idPedido end)/count(distinct idPedido) as pct_PedidoMG,  
+  count(CASE WHEN descUF = 'MS' then idPedido end)/count(distinct idPedido) as pct_PedidoMS,  
+  count(CASE WHEN descUF = 'MT' then idPedido end)/count(distinct idPedido) as pct_PedidoMT,  
+  count(CASE WHEN descUF = 'PA' then idPedido end)/count(distinct idPedido) as pct_PedidoPA,  
+  count(CASE WHEN descUF = 'PB' then idPedido end)/count(distinct idPedido) as pct_PedidoPB,  
+  count(CASE WHEN descUF = 'PE' then idPedido end)/count(distinct idPedido) as pct_PedidoPE,  
+  count(CASE WHEN descUF = 'PI' then idPedido end)/count(distinct idPedido) as pct_PedidoPI,  
+  count(CASE WHEN descUF = 'PR' then idPedido end)/count(distinct idPedido) as pct_PedidoPR,  
+  count(CASE WHEN descUF = 'RJ' then idPedido end)/count(distinct idPedido) as pct_PedidoRJ,  
+  count(CASE WHEN descUF = 'RN' then idPedido end)/count(distinct idPedido) as pct_PedidoRN,  
+  count(CASE WHEN descUF = 'RO' then idPedido end)/count(distinct idPedido) as pct_PedidoRO,  
+  count(CASE WHEN descUF = 'RR' then idPedido end)/count(distinct idPedido) as pct_PedidoRR,  
+  count(CASE WHEN descUF = 'RS' then idPedido end)/count(distinct idPedido) as pct_PedidoRS,  
+  count(CASE WHEN descUF = 'SC' then idPedido end)/count(distinct idPedido) as pct_PedidoSC,  
+  count(CASE WHEN descUF = 'SE' then idPedido end)/count(distinct idPedido) as pct_PedidoSE,  
+  count(CASE WHEN descUF = 'SP' then idPedido end)/count(distinct idPedido) as pct_PedidoSP,  
+  count(CASE WHEN descUF = 'TO' then idPedido end)/count(distinct idPedido) as pct_PedidoTO
+
+
+FROM tb_join
+GROUP BY idVendedor
+
 )
 
-SELECT *
-FROM tb_join
+SELECT 
+    '2018-01-01' as dtReference,
+    *   
+       
+FROM tb_group
 
 -- COMMAND ----------
 
